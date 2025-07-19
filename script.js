ac095b822f3faeae20058feab6b60ae1e2720819
Sanskar Ghimire <sanskarghimire1000@gmail.com>
2025-07-19 20:16:30 +0545

    added basic functionality

---
+++ script.js
@@ -0,0 +1,6 @@
+const decreasebtn = document.getElementById("decrease");
+const resetbtn = document.getElementById("reset");
+const increasebtn = document.getElementById("increase");
+const countlabel = document.getElementById("mynum");
+
+let count = 0;
