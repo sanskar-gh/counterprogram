837a1ba531c801eeadbd9dfdd7d3a1bafeea2b93
Sanskar Ghimire <sanskarghimire1000@gmail.com>
2025-07-19 20:17:09 +0545

    added full functionality

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ const increasebtn = document.getElementById("increase");
 const countlabel = document.getElementById("mynum");
 
 let count = 0;
+
+decreasebtn.onclick = () => {
+   count--;
+   countlabel.textContent = count;
+}
+increasebtn.onclick = () => {
+   count++;
+   countlabel.textContent = count;
+}
+resetbtn.onclick = () => {
+   count = 0;
+   countlabel.textContent = count;
+}
